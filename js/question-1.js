05f394b9cd6557ef8567dd2d3082e4c4e4293ea8
mathildeew <mathildewiik@hotmail.com>
2022-04-20 15:18:35 +0200

    question 1

---
+++ js/question-1.js
@@ -0,0 +1,12 @@
+// question 1: convert the function below to an arrow function:
+
+// function getRemainder(a, b) {
+//   return a % b;
+// }
+
+const getRemainder = (a, b) => {
+  return a % b;
+};
+
+const result = getRemainder(13, 5);
+console.log(result);
